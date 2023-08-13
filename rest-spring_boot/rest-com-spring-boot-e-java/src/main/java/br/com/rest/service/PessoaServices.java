bd3d7c7e9c911c3ea8b37712e414eb2f5e2cda88
Felipe Ferreira <flpferreira1999@gmail.com>
2023-08-13 10:05:47 -0300

    CRUD pessoa

---
+++ rest-spring_boot/rest-com-spring-boot-e-java/src/main/java/br/com/rest/service/PessoaServices.java
@@ -0,0 +1,88 @@
+package br.com.rest.service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rest.data.vo.v1.PessoaVO;
+import br.com.rest.exception.ResourceNotFoundException;
+import br.com.rest.mapper.DozerMapper;
+import br.com.rest.model.Pessoa;
+import br.com.rest.repositories.PessoaRepository;
+
+@Service
+public class PessoaServices {
+	
+	private Logger logger = Logger.getLogger(PessoaServices.class.getName());
+	
+	@Autowired
+	PessoaRepository repository;
+	
+	public List<PessoaVO> findAll() {
+		logger.info("todas as pessoas");
+	
+		return DozerMapper.parseListObject(repository.findAll(), PessoaVO.class);
+	}
+	
+
+
+	public PessoaVO findById(Long id) {
+		logger.info("Encontrando uma Pessoa!");
+		
+		var entity = repository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("nenhum registro encontrado para esse ID"));
+		return DozerMapper.parseObject(entity, PessoaVO.class);
+	}
+
+	
+	
+	public PessoaVO criar(PessoaVO pessoa) {
+		logger.info("criando uma pessoa");
+		var entity = DozerMapper.parseObject(pessoa, Pessoa.class);
+		var vo = DozerMapper.parseObject(repository.save(entity), PessoaVO.class);
+		return vo;
+	}
+	
+	
+	public PessoaVO atualizar(PessoaVO pessoa) {
+		logger.info("atualizando uma pessoa");
+		
+		var entity = repository.findById(pessoa.getId())
+		.orElseThrow(()-> new ResourceNotFoundException("nenhum registro encontrado para esse ID"));
+		
+		
+		entity.setNome(pessoa.getNome());
+		entity.setSobrenome(pessoa.getSobrenome());
+		entity.setEndereco(pessoa.getEndereco());
+		entity.setSexo(pessoa.getSexo());
+		
+		var vo = DozerMapper.parseObject(repository.save(entity), PessoaVO.class);
+		return vo;
+	}
+	
+	
+	public void delete(Long id) {
+		logger.info("excluindo uma pessoa");
+
+		var entity = repository.findById(id)
+		.orElseThrow(()-> new ResourceNotFoundException("nenhum registro encontrado para esse ID"));
+		repository.delete(entity);
+	}
+	
+	
+/*
+	private Pessoa mockPessoa(int i) {
+
+		Pessoa pessoa = new Pessoa();
+		pessoa.setId(counter.incrementAndGet());
+		pessoa.setNome("Pessoa nome " + i);
+		pessoa.setSobrenome("Sobrenome " + i);
+		pessoa.setEndereco("Algum endereço do Brasil " + i);
+		pessoa.setSexo("Masculino");
+		return pessoa;
+	}
+	*/
+}
+
---
+++ rest-spring_boot/rest-com-spring-boot-e-java/src/test/java/br/com/rest/unittests/mapper/DozerConverterTest.java
@@ -0,0 +1,99 @@
+package br.com.rest.unittests.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.rest.mapper.DozerMapper;
+import br.com.rest.unittests.mapper.mocks.MockPerson;
+import br.com.rest.data.vo.v1.PessoaVO;
+import br.com.rest.model.Pessoa;
+
+public class DozerConverterTest {
+    
+    MockPerson inputObject;
+
+    @BeforeEach
+    public void setUp() {
+        inputObject = new MockPerson();
+    }
+
+    @Test
+    public void parseEntityToVOTest() {
+        PessoaVO output = DozerMapper.parseObject(inputObject.mockEntity(), PessoaVO.class);
+        assertEquals(Long.valueOf(0L), output.getId());
+        assertEquals("First Name Test0", output.getNome());
+        assertEquals("Last Name Test0", output.getSobrenome());
+        assertEquals("Addres Test0", output.getEndereco());
+        assertEquals("Male", output.getSexo());
+    }
+
+    @Test
+    public void parseEntityListToVOListTest() {
+        List<PessoaVO> outputList = DozerMapper.parseListObject(inputObject.mockEntityList(), PessoaVO.class);
+        PessoaVO outputZero = outputList.get(0);
+        
+        assertEquals(Long.valueOf(0L), outputZero.getId());
+        assertEquals("First Name Test0", outputZero.getNome());
+        assertEquals("Last Name Test0", outputZero.getSobrenome());
+        assertEquals("Addres Test0", outputZero.getEndereco());
+        assertEquals("Male", outputZero.getSexo());
+        
+        PessoaVO outputSeven = outputList.get(7);
+        
+        assertEquals(Long.valueOf(7L), outputSeven.getId());
+        assertEquals("First Name Test7", outputSeven.getNome());
+        assertEquals("Last Name Test7", outputSeven.getSobrenome());
+        assertEquals("Addres Test7", outputSeven.getEndereco());
+        assertEquals("Female", outputSeven.getSexo());
+        
+        PessoaVO outputTwelve = outputList.get(12);
+        
+        assertEquals(Long.valueOf(12L), outputTwelve.getId());
+        assertEquals("First Name Test12", outputTwelve.getNome());
+        assertEquals("Last Name Test12", outputTwelve.getSobrenome());
+        assertEquals("Addres Test12", outputTwelve.getEndereco());
+        assertEquals("Male", outputTwelve.getSexo());
+    }
+
+    @Test
+    public void parseVOToEntityTest() {
+    	Pessoa output = DozerMapper.parseObject(inputObject.mockVO(), Pessoa.class);
+        assertEquals(Long.valueOf(0L), output.getId());
+        assertEquals("First Name Test0", output.getNome());
+        assertEquals("Last Name Test0", output.getSobrenome());
+        assertEquals("Addres Test0", output.getEndereco());
+        assertEquals("Male", output.getSexo());
+    }
+
+    @Test
+    public void parserVOListToEntityListTest() {
+        List<Pessoa> outputList = DozerMapper.parseListObject(inputObject.mockVOList(), Pessoa.class);
+        Pessoa outputZero = outputList.get(0);
+        
+        assertEquals(Long.valueOf(0L), outputZero.getId());
+        assertEquals("First Name Test0", outputZero.getNome());
+        assertEquals("Last Name Test0", outputZero.getSobrenome());
+        assertEquals("Addres Test0", outputZero.getEndereco());
+        assertEquals("Male", outputZero.getSexo());
+        
+        Pessoa outputSeven = outputList.get(7);
+        
+        assertEquals(Long.valueOf(7L), outputSeven.getId());
+        assertEquals("First Name Test7", outputSeven.getNome());
+        assertEquals("Last Name Test7", outputSeven.getSobrenome());
+        assertEquals("Addres Test7", outputSeven.getEndereco());
+        assertEquals("Female", outputSeven.getSexo());
+        
+        Pessoa outputTwelve = outputList.get(12);
+        
+        assertEquals(Long.valueOf(12L), outputTwelve.getId());
+        assertEquals("First Name Test12", outputTwelve.getNome());
+        assertEquals("Last Name Test12", outputTwelve.getSobrenome());
+        assertEquals("Addres Test12", outputTwelve.getEndereco());
+        assertEquals("Male", outputTwelve.getSexo());
+    }
+}
